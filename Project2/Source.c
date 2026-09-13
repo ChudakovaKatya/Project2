225595fad59b857211ccd055894d68ecfa709120
Катюха <@VORN-NOTE-IT2>
2026-09-13 22:29:57 +0300

    Добавьте файлы проекта.

---
+++ Project2/Source.c
@@ -0,0 +1,30 @@
+﻿#include <locale.h>
+
+#include <stdio.h>
+
+void main()
+
+{
+
+	setlocale(LC_ALL, "UTF-8"); 
+
+	puts("         _     ");
+	puts("     /\\ | | /\\");
+	puts("    /  \\| |/  \\");
+	puts("   (     V     )");
+	puts("    \\         /   ");
+	puts("     \\       /");
+	puts("      \\     /");
+	puts("       \\   /");
+	puts("         V");
+	puts("         |  /|");
+	puts("         |_/ |");
+	puts("     |\\  |__/");
+	puts("     | \\_|");
+	puts("     \\ _ |");
+	puts("         |");
+	puts("         |");
+	puts("         |");
+	
+
+}
